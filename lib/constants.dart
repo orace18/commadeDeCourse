939fac19cb094e393bc2dba5d96340d841c7a08b
Ariel Carmen <arielahognisse@gmail.com>
2023-12-28 02:18:34 +0100

    starting on fedapayment

--- lib/constants.dart
+++ lib/constants.dart
@@ -27,16 +27,16 @@ const default2xSizedBox = SizedBox(height: 20, width: 20,);
 void navigateToHome(int roleId){
   switch (roleId) {
     case 1:
-      Get.toNamed("/marchand");
+      Get.toNamed("/");
       break;
     case 2:
-      Get.toNamed("/driver");
+      Get.toNamed("/marchand");
       break;
     case 3:
-      Get.toNamed("/passager");
+      Get.toNamed("/driver");
       break;
     case 4:
-      Get.toNamed("/");
+      Get.toNamed("/passager");
       break;
     default:
       break;
